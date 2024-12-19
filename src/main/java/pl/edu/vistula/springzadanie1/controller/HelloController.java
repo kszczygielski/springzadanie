0ee38c68cf3c3aba3dc5d39fb94ee37487a94e4d
Kamil <kkamilszczygielski@gmail.com>
2024-12-19 18:11:18 +0100

    first commit

--- src/main/java/pl/edu/vistula/springzadanie1/controller/HelloController.java
+++ src/main/java/pl/edu/vistula/springzadanie1/controller/HelloController.java
@@ -1,4 +1,25 @@
-package pl.edu.vistula.springzadanie1.controller;
+package pl.edu.vistula.zadanie1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+//@RestController
 public class HelloController {
+
+    @GetMapping(value = "/")
+    public String hello() {
+        return "Hello Vistula, in my first Spring controller.";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 }
+
+// http://localhost:8080/greeting?name=Vistula
